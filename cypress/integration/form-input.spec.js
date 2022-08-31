40314cacc9c45ded77dc47ed7e83552ed81ec692
Paulo Marchi <marchipaulo@gmail.com>
2022-08-31 09:55:28 -0300

    Criação do cenario 'Accepts input'

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -1,6 +1,20 @@
 describe('Form input', () => {
-  it('Focuses the input on load', () => {
+  beforeEach(() => {
     cy.visit('/');
-    cy.focused().should('have.id', 'title');
+  });
+
+  it('Focuses the input on load', () => {
+    //focused -> procura o campo com foca quando a tela abre
+    cy.focused()
+      .should('have.id', 'title');
+  });
+
+  //.only -> faz com que apenas esse cenario "it" rode, ignorando os demais deste arquivo (it.only)
+  it('Accepts input', () => {
+    const value = 'New todo';
+    //type -> insere dados no campo resgatado pelo get
+    cy.get('#title')
+      .type(value)
+      .should('have.value', value);
   });
 });
